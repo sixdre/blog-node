8807029cdd6e6488b8cb7e8683c20a3f228df54c
徐浩 <1969716722@qq.com>
2017-12-28 16:21:20 +0800

    添加 跨域处理

--- app.js
+++ app.js
@@ -18,6 +18,20 @@ global.CONFIG = JSON.parse(fs.readFileSync('./config/settings.json').toString())
 
 const app = global.app = express();
 
+
+
+app.all('*', function(req, res, next) {  
+    res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , x-access-token');
+	res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+	if (req.method == 'OPTIONS') {
+	   res.send(200); 
+	}
+	else {
+	   next();
+	}
+});  
+
 app.locals.moment = moment;
 const mongoStroe = connectMongo(session); 
 app.use(cookieParser());
