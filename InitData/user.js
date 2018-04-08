267fd4b78d1d2f05788a1074c0401f7ee3383477
徐浩 <1969716722@qq.com>
2018-04-08 17:22:21 +0800

    修改初始化分类 和用户

--- InitData/user.js
+++ InitData/user.js
@@ -11,14 +11,28 @@
  		email:'123@qq.com',
  		password:'123456',
  		avatar:'https://sfault-avatar.b0.upaiyun.com/161/227/1612276764-55f6bdd353b39_big64',
- 		isAdmin:'false'
+ 		isAdmin:'true'
  	},
  	{
- 		username:'徐先生',
+ 		username:'徐浩',
  		email:'123@qq.com',
  		password:'123456',
  		avatar:'https://sfault-avatar.b0.upaiyun.com/161/227/1612276764-55f6bdd353b39_big64',
- 		isAdmin:'false'
+ 		isAdmin:'true'
+ 	},
+ 	{
+ 		username:'benben',
+ 		email:'123@qq.com',
+ 		password:'123456',
+ 		avatar:'https://sfault-avatar.b0.upaiyun.com/161/227/1612276764-55f6bdd353b39_big64',
+ 		isAdmin:'true'
+ 	},
+ 	{
+ 		username:'测试',
+ 		email:'123@qq.com',
+ 		password:'123456',
+ 		avatar:'https://sfault-avatar.b0.upaiyun.com/161/227/1612276764-55f6bdd353b39_big64',
+ 		isAdmin:'true'
  	}
 ]
 
